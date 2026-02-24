513ec61c07e7301cb76ae2f2bb407d19a82297a4
parthagarkar22 <parthagarkar19@gmail.com>
2026-02-24 17:15:36 +0530

    Added build.sh for Render

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
