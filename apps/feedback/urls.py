4316e8f53ad69bb6979370cc9dfdca8b51b446c3
parthagarkar22 <parthagarkar19@gmail.com>
2026-02-25 18:33:59 +0530

    remove gdal in setting

--- apps/feedback/urls.py
+++ apps/feedback/urls.py
@@ -1,6 +1,16 @@
+# from django.urls import path
+# from .views import FeedbackCreateView,FeedbackAdminList
+
+# urlpatterns = [
+#     path("", FeedbackCreateView.as_view()),
+
+# ]
+
+
 from django.urls import path
-from .views import FeedbackCreateView
+from .views import FeedbackCreateView, FeedbackAdminListView
 
 urlpatterns = [
-    path("", FeedbackCreateView.as_view()),
+    path("", FeedbackCreateView.as_view(), name="create-feedback"),
+    path("admin/list/", FeedbackAdminListView.as_view(), name="admin-feedback-list"),
 ]
--- config/settings.py
+++ config/settings.py
@@ -237,8 +237,8 @@ load_dotenv(BASE_DIR / ".env")
 # =========================
 # GDAL / GEOS (Windows GIS)
 # =========================
-GDAL_LIBRARY_PATH = r"C:\Program Files\GDAL\gdal.dll"
-GEOS_LIBRARY_PATH = r"C:\Program Files\GDAL\geos_c.dll"
+# GDAL_LIBRARY_PATH = r"C:\Program Files\GDAL\gdal.dll"
+# GEOS_LIBRARY_PATH = r"C:\Program Files\GDAL\geos_c.dll"
 
 # =========================
 # BASIC CONFIG
@@ -348,7 +348,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # =========================
 # DATABASE (Using dj_database_url)
 # =========================
-import dj_database_url
+# import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.parse(
